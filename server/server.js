8a51228a39157e4ddd7c23cb29712225d2ed8ba7
Gautam Yajaman <g.yajaman@gmail.com>
2025-02-20 15:35:47 +0000

    server routing fixed

--- server/server.js
+++ server/server.js
@@ -14,6 +14,11 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
+
 // Routes
 app.use('/api/messages', messageRoutes);
 
